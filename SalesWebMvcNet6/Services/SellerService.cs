41df52b09a2f751cdd70c336686738481b715d2d
luizh-gsoares <luizhenriquesoares18@hotmail.com>
2022-11-22 10:44:41 -0300

    Added support to foreign key not null

--- SalesWebMvcNet6/Services/SellerService.cs
+++ SalesWebMvcNet6/Services/SellerService.cs
@@ -20,6 +20,7 @@ namespace SalesWebMvcNet6.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
