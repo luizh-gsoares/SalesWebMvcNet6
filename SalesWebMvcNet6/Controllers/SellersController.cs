851174c3d014a03d2790b4483bc148a344a40847
luizh-gsoares <luizhenriquesoares18@hotmail.com>
2022-11-22 10:24:42 -0300

    Added seller form

--- SalesWebMvcNet6/Controllers/SellersController.cs
+++ SalesWebMvcNet6/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvcNet6.Models;
 using SalesWebMvcNet6.Services;
 using System.Drawing.Text;
 
@@ -17,6 +18,19 @@ namespace SalesWebMvcNet6.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- SalesWebMvcNet6/Services/SellerService.cs
+++ SalesWebMvcNet6/Services/SellerService.cs
@@ -17,5 +17,11 @@ namespace SalesWebMvcNet6.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
