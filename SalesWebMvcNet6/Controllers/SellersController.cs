9eea41b3d6a122a537c8f2cdf4e221007a0dd01d
luizh-gsoares <luizhenriquesoares18@hotmail.com>
2022-11-22 11:58:19 -0300

    Added delete seller

--- SalesWebMvcNet6/Controllers/SellersController.cs
+++ SalesWebMvcNet6/Controllers/SellersController.cs
@@ -11,7 +11,7 @@ namespace SalesWebMvcNet6.Controllers
         private readonly SellerService _sellerService;
         private readonly DepartmentService _departmentService;
 
-        public SellersController (SellerService sellerService, DepartmentService departmentService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
             _departmentService = departmentService;
@@ -26,7 +26,7 @@ namespace SalesWebMvcNet6.Controllers
         public IActionResult Create()
         {
             var departments = _departmentService.FindAll();
-            var ViewModel = new SellerFormViewModel {Departments = departments};
+            var ViewModel = new SellerFormViewModel { Departments = departments };
             return View(ViewModel);
         }
 
@@ -35,6 +35,31 @@ namespace SalesWebMvcNet6.Controllers
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
