673a568b9e6bcf57b70b45e2c8c8189cfe545a0e
luizh-gsoares <luizhenriquesoares18@hotmail.com>
2022-11-22 08:57:37 -0300

    SellersController

---
+++ SalesWebMvcNet6/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvcNet6.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
