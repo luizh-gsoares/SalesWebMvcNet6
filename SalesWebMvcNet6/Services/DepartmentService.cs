94f92f1e745e8daccce73e885274279eb02e6756
luizh-gsoares <luizhenriquesoares18@hotmail.com>
2022-11-23 12:00:00 -0300

    Asynchronous operations using Tasks (async, await)

--- SalesWebMvcNet6/Services/DepartmentService.cs
+++ SalesWebMvcNet6/Services/DepartmentService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMvcNet6.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvcNet6.Data;
 using SalesWebMvcNet6.Models;
 
 namespace SalesWebMvcNet6.Services
@@ -12,9 +13,9 @@ namespace SalesWebMvcNet6.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
